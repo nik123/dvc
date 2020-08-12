4781e185b36dae4925ed2507e2bf8c91355b3485
Nikita Kodenko <kodenkonikita@gmail.com>
2020-08-13 01:04:38 +0700

    ui: '-j' option now mentions docs

--- dvc/command/gc.py
+++ dvc/command/gc.py
@@ -123,7 +123,7 @@ def add_parser(subparsers, parent_parser):
         "-j",
         "--jobs",
         type=int,
-        help="Number of jobs to run simultaneously.",
+        help="Number of jobs to run simultaneously. Refer to docs to get default value.",
         metavar="<number>",
     )
     gc_parser.add_argument(
